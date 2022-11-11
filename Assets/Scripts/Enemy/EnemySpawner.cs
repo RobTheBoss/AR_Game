c8710797346fd3719d225242f4375a5e102dcbe1
RobTheBoss <Robertbato825@gmail.com>
2022-11-11 04:47:50 -0500

    Changed some mechanics (read description)
    -Enemies now always spawn on screen making locating them not an issue
    
    -To compensate, enemies spawn much faster
    
    -Spawn times are now random. Instead of always spawning at a set time, enemies spawn every 1.5-4.5 seconds

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,33 +4,38 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] float spawnCooldown;
+    [SerializeField] float spawnCooldownMin;
+    [SerializeField] float spawnCooldownMax;
+
     [SerializeField] float spawnDistance;
     [SerializeField] GameObject[] enemies;
 
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", 0, spawnCooldown);
+        Invoke("SpawnEnemy", Random.Range(spawnCooldownMin, spawnCooldownMax));
     }
 
     public void SpawnEnemy()
     {
         int i = Random.Range(0, enemies.Length);
         Instantiate(enemies[i], GetSpawnPoint(), Quaternion.identity);
+
+        Invoke("SpawnEnemy", Random.Range(spawnCooldownMin, spawnCooldownMax));
     }
 
     private Vector3 GetSpawnPoint()
     {
-        Vector3 spawnDirection = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)).normalized;
+        Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector3 playerFaceDirection = GameObject.FindGameObjectWithTag("Player").transform.forward;
 
-        Vector3 finalSpawnPoint = new Vector3(0,0,0) + (spawnDirection * spawnDistance);
+        float angleFromFaceDirection = Random.Range(-25.0f, 25.0f);
+        Vector3 finalSpawnPoint = playerPos + Quaternion.Euler(0, angleFromFaceDirection, 0) * playerFaceDirection * spawnDistance;
 
         if (ARController.floorHeight != 0)
             finalSpawnPoint.y = ARController.floorHeight;
         else
             finalSpawnPoint.y = -0.5f;
 
-        Debug.Log("Enemy Spawn Found");
         return finalSpawnPoint;
     }
 }
